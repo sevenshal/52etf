851b72930c65bc63f5b697a392cfec80a9f22fb9
sevenshal <sevenshal@gmail.com>
2025-07-10 15:30:56 +0800

    no cache

--- backend/backend/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="quant",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    python_requires=">=3.6",
-    include_package_data=True,
-) 
