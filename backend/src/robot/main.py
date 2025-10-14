cfcbf030c326ae82ba2dfc8bd1e73f9b8f12a981
sevenshal <sevenshal@163.com>
2025-10-14 14:01:54 +0800

    moidfy

--- backend/src/robot/main.py
+++ backend/src/robot/main.py
@@ -22,8 +22,8 @@ cnn_fear_index_scraper = CNNFearGreedIndexScraper()
 market_signal_analyzer = MarketSignalAnalyzer(longPortService)
 
 def robot():
-  evc_manager.fetch_and_stocks(force_fetch=True)
-  # etf_manager.analyze_all_fair_value()
+  evc_manager.fetch_and_stocks(force_fetch=False)
+  etf_manager.analyze_all_fair_value()
   # cnn_fear_index_scraper.fetch_data_and_save()
   # etf_manager.calculate_all_emotions()
   market_signal_analyzer.analyze()
