5de8550d8593f717613bcfc9e3ef5d41f8c3c592
sevenshal <sevenshal@gmail.com>
2026-01-03 13:18:31 +0800

    modify

--- frontend/src/pages/PortfolioCopyTrading.jsx
+++ frontend/src/pages/PortfolioCopyTrading.jsx
@@ -325,7 +325,7 @@ const PortfolioCopyTrading = () => {
                 onOk={() => form.submit()}
                 width={700}
             >
-                <Form form={form} layout="vertical" onFinish={handleSave} initialValues={{ enabled: true, cron_rule: '0 8 * * *', tracking_error_pct: 10, total_position_ratio: 100, price_buffer_pct: 0.5 }}>
+                <Form form={form} layout="vertical" onFinish={handleSave} initialValues={{ enabled: true, cron_rule: '0 8 * * *', tracking_error_pct: 5, total_position_ratio: 100 }}>
                     <Row gutter={16}>
                         <Col span={24}>
                             <div style={{ display: 'flex', alignItems: 'center', marginBottom: 24, marginTop: 12 }}>
@@ -369,24 +369,19 @@ const PortfolioCopyTrading = () => {
                     </Row>
 
                     <Row gutter={16}>
-                        <Col span={6}>
+                        <Col span={8}>
                             <Form.Item name="total_position_ratio" label="总仓位比例 (%)">
                                 <InputNumber style={{ width: '100%' }} min={0} max={100} />
                             </Form.Item>
                         </Col>
-                        <Col span={6}>
+                        <Col span={8}>
                             <Form.Item name="total_amount" label="总金额 (优先)">
                                 <InputNumber style={{ width: '100%' }} />
                             </Form.Item>
                         </Col>
-                        <Col span={6}>
+                        <Col span={8}>
                             <Form.Item name="tracking_error_pct" label="跟踪误差 (%)">
                                 <InputNumber style={{ width: '100%' }} min={0} max={100} />
-                            </Form.Item>
-                        </Col>
-                        <Col span={6}>
-                            <Form.Item name="price_buffer_pct" label="价格缓冲 (%)">
-                                <InputNumber style={{ width: '100%' }} min={0} step={0.1} />
                             </Form.Item>
                         </Col>
                     </Row>
