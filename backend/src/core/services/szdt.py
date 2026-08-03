6ed358bb2064bedfdff18a295288099553902076
sevenshal <sevenshal@gmail.com>
2026-08-03 15:25:31 +0800

    modify szdt

--- backend/src/core/services/szdt.py
+++ backend/src/core/services/szdt.py
@@ -33,7 +33,7 @@ class SZDTService:
         self.base_url = "https://szdt.tech/api"
         self.logger = logging.getLogger("SZDTService")
         self.secret_key = "X9XuQ89fjX4nq4FbdDM4LjVMYvDTsVVh"  # 从JS代码中提取的密钥
-        self.auth_code = 'meHTJgAi8hEausoh4ACj5FzMeOelDSIm:34f95cf6f11365e8376344081b0c9d472257314f'
+        self.auth_code = 'meHTJgAi8hEausoh4ACj5FzMeOelDSIm:119eeaa30e14c7fbbcedf3b63ed6a9fedbb78d25'
         self.partner_auth = 'meHTJgAi8hEausoh4ACj5FzMeOelDSIm'  # 合作伙伴认证码
         self.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"
         self.timeout_seconds = _load_timeout_seconds()
